d55a1551ad0ee5113d2e18d13e7ad82c56244b23
mustafa <muhammdmustafa5762@gmail.com>
2022-12-09 23:27:50 +0500

    pro

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -23,6 +23,17 @@ module.exports ={
     },
     postproducts: async function(req, res){
         try {
+          const cr1 = JSON.parse(req.body.color1)
+          const cr2 = JSON.parse(req.body.color2)
+          const cr3 = JSON.parse(req.body.color3)
+          const sz1 = JSON.parse(req.body.size1)
+          const sz2 = JSON.parse(req.body.size2)
+          const sz3 = JSON.parse(req.body.size3)
+          const sz4 = JSON.parse(req.body.size4)
+          const sz5 = JSON.parse(req.body.size5)
+          const sz6 = JSON.parse(req.body.size6)
+
+
           const newpro = new Product({
             name: req.body.name,
             price: req.body.price,
@@ -32,15 +43,15 @@ module.exports ={
             additional_info: req.body.additional_info,
             category: req.body.category,
             onHomePage: req.body.onHomePage,
-            colors: req.body.colors,
-            sizes: req.body.sizes,
+            colors: [{name: cr1.name, class: cr1.class, selectedClass: cr1.selectedClass},{name: cr2.name, class: cr2.class, selectedClass: cr2.selectedClass},{name: cr3.name, class: cr3.class, selectedClass: cr3.selectedClass}],
+            sizes: [{ name: sz1.name, inStock: sz1.inStock },{ name: sz2.name, inStock: sz2.inStock },{ name: sz3.name, inStock: sz3.inStock },{ name: sz4.name, inStock: sz4.inStock },{ name: sz5.name, inStock: sz5.inStock },{ name: sz6.name, inStock: sz6.inStock }],
             tags: req.body.tags,
             added_by: req.body.added_by,
             views: 0,
             image: req.file.path,
           });
-          const saves = await newpro.save();
-          res.send(saves);
+          const prods = await newpro.save();
+          res.send(prods)
         } catch (error) {
           res.send(error);
         }
